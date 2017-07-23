018b9ee8172e21f0bd9baaf82dc71c6989bbe456
Dimitri Merejkowsky <d.merej@gmail.com>
2017-07-23 21:05:33 +0200

    Static files are now found in yulist/ directory

--- yulist/builder.py
+++ yulist/builder.py
@@ -1,3 +1,4 @@
+import pathlib
 import shutil
 
 from yulist.parser import Parser
@@ -14,23 +15,30 @@ class Builder:
                                    output_format=self.output_format)
 
     def build(self):
+        print("Building site ...")
         self.build_pages()
         self.copy_static_files()
+        print("Done")
 
     def build_pages(self):
+        n = 0
         for page in self.parser.parse():
             out_path = self.dest_path / page["path"]
             out_path = out_path.with_suffix("." + self.output_format)
-            print("::", out_path.relative_to(self.dest_path))
+            n += 1
             out_path.parent.mkdir(parents=True, exist_ok=True)
             page_content = self.generator.generate_page(page)
             out_path.write_text(page_content)
+        print("*", "Written", n, "pages")
 
     def copy_static_files(self):
-        static_path = self.src_path / "static"
+        this_path = pathlib.Path(__file__).parent
+        static_path = this_path / "static"
+        n = 0
         for entry in static_path.glob("**/*"):
-            rel_path = entry.relative_to(self.src_path)
+            rel_path = entry.relative_to(this_path)
             full_dest = self.dest_path / rel_path
             full_dest.parent.mkdir(parents=True, exist_ok=True)
-            print("->", rel_path)
             shutil.copy(entry, full_dest)
+            n += 1
+        print("*", "Copied", n, "static file(s)")
