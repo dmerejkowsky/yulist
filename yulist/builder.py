162b9769db99eae248878381bf43f08d3cc09fe2
Dimitri Merejkowsky <d.merej@gmail.com>
2017-07-23 17:44:02 +0200

    Handle static files

--- yulist/builder.py
+++ yulist/builder.py
@@ -1,3 +1,5 @@
+import shutil
+
 import yulist.parser
 import yulist.generator
 
@@ -11,10 +13,23 @@ class Builder:
         self.generator = yulist.generator.Generator(output_format=self.output_format)
 
     def build(self):
+        self.build_pages()
+        self.copy_static_files()
+
+    def build_pages(self):
         for page in self.parser.parse():
             out_path = self.dest_path / page["path"]
             out_path = out_path.with_suffix("." + self.output_format)
+            print("::", out_path.relative_to(self.dest_path))
             out_path.parent.mkdir(parents=True, exist_ok=True)
             page_content = self.generator.generate_page(page)
             out_path.write_text(page_content)
-            print(out_path.relative_to(self.dest_path))
+
+    def copy_static_files(self):
+        static_path = self.src_path / "static"
+        for entry in static_path.glob("**/*"):
+            rel_path = entry.relative_to(self.src_path)
+            full_dest = self.dest_path / rel_path
+            full_dest.parent.mkdir(parents=True, exist_ok=True)
+            print("->", rel_path)
+            shutil.copy(entry, full_dest)
--- yulist/test/test_main.py
+++ yulist/test/test_main.py
@@ -8,4 +8,6 @@ def test_build_site(tmp_path, example_path):
     args = [example_path, tmp_path]
     yulist.main.build(src_path=example_path, dest_path=tmp_path, output_format="html")
     index_html = tmp_path / "index.html"
+    css = tmp_path / "static" / "style.css"
     assert index_html.exists()
+    assert css.exists()
