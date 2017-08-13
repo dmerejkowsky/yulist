8fe815630ac6e23575f7285335b98b8003edc2ec
Dimitri Merejkowsky <d.merej@gmail.com>
2017-08-13 16:48:19 +0200

    test_main: remove unused variable

--- yulist/test/test_main.py
+++ yulist/test/test_main.py
@@ -5,7 +5,6 @@ import yulist.main
 
 
 def test_build_site(tmp_path, example_path):
-    args = [example_path, tmp_path]
     yulist.main.build(src_path=example_path, dest_path=tmp_path)
     index_html = tmp_path / "index.html"
     css = tmp_path / "static" / "style.css"
